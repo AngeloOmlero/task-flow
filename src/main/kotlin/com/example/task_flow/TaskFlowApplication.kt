5d1328d8d73c609144b3ebca1447695c0ead100c
Angelo Omlero <aomlero@vauldex.com>
2025-12-04 08:20:14 +0800

    Initial commit

---
+++ src/main/kotlin/com/example/task_flow/TaskFlowApplication.kt
@@ -0,0 +1,11 @@
+package com.example.task_flow
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TaskFlowApplication
+
+fun main(args: Array<String>) {
+	runApplication<TaskFlowApplication>(*args)
+}
