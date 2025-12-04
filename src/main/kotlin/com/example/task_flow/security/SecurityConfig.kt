89f11843c8c2b9f624f4e74aff89f24e3996d1b5
Angelo Omlero <aomlero@vauldex.com>
2025-12-04 09:10:41 +0800

    feat(security): add SecurityConfig for authentication and CORS setup
    - Configured HTTP security with JWT authentication filter
    - Permitted public access to /api/auth/**
    - Authenticated access to  /api/boards/**, /api/tasks/**, and /ws/**
    - Enabled CORS with allowed origins, methods, and headers
    - Added password encoding using BCryptPasswordEncoder
    - Exposed AuthenticationManager as a bean for authentication handling of API error responses

---
+++ src/main/kotlin/com/example/task_flow/security/SecurityConfig.kt
@@ -0,0 +1,64 @@
+package com.example.task_flow.security
+
+
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.security.authentication.AuthenticationManager
+import org.springframework.security.config.Customizer
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+
+@Configuration
+@EnableWebSecurity
+
+class SecurityConfig(private val jwtAuthFilter: JwtAuthFilter
+) {
+
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder {
+        return BCryptPasswordEncoder()
+    }
+
+    @Bean
+    fun authenticationManager(authenticationConfiguration: AuthenticationConfiguration): AuthenticationManager {
+        return authenticationConfiguration.authenticationManager
+    }
+
+    @Bean
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .csrf { it.disable() }
+            .authorizeHttpRequests { auth ->
+                auth
+                    .requestMatchers("/api/auth/**",).permitAll()
+                    .requestMatchers("/ws/**","/api/boards/**","/api/tasks/**").authenticated()
+                    .anyRequest().authenticated()
+            }
+            .cors(Customizer.withDefaults())
+            .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter::class.java)
+
+
+        return http.build()
+    }
+
+    @Bean
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        val configuration = CorsConfiguration()
+        configuration.allowedOrigins = listOf("http://localhost:5173","http://127.0.0.1:5500", "http://localhost:3000","http://localhost:63342" )
+        configuration.allowedMethods = listOf("GET", "POST", "PUT", "DELETE")
+        configuration.allowedHeaders = listOf("*")
+        configuration.allowCredentials = true
+        val source = UrlBasedCorsConfigurationSource()
+        source.registerCorsConfiguration("/**", configuration)
+        return source
+    }
+}
