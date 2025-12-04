b8267ef90f1537d7bf715bb07993eedd6d7938e3
Angelo Omlero <aomlero@vauldex.com>
2025-12-04 09:53:55 +0800

    feat(websocket): add BroadcastService
    - Implemented BroadcastService to send real-time updates to WebSocket subscribers
    - Prepared methods for broadcasting board, task, and comment changes

---
+++ src/main/kotlin/com/example/task_flow/service/BroadcastService.kt
@@ -0,0 +1,30 @@
+package com.example.task_management.service
+
+import org.springframework.messaging.simp.SimpMessagingTemplate
+import org.springframework.stereotype.Service
+
+@Service
+class BroadcastService(
+    private val messagingTemplate: SimpMessagingTemplate
+) {
+    fun sendBoardsUpdate(boardId: Long, message: Any) {
+        messagingTemplate.convertAndSend("/topic/boards/$boardId", message)
+    }
+
+    fun sendBoardsCollectionUpdate(message: Any) {
+        messagingTemplate.convertAndSend("/topic/boards", message)
+    }
+
+    fun sendTaskUpdate(boardId: Long, taskId: Long, message: Any) {
+        messagingTemplate.convertAndSend("/topic/boards/$boardId/tasks", message)
+        messagingTemplate.convertAndSend("/topic/tasks/$taskId", message)
+    }
+
+    fun sendCommentUpdate(taskId: Long, message: Any) {
+        messagingTemplate.convertAndSend("/topic/tasks/$taskId/comments", message)
+    }
+
+    fun sendToUserBoardsTopic(username: String, message: Any) {
+        messagingTemplate.convertAndSendToUser(username, "/topic/boards", message)
+    }
+}
